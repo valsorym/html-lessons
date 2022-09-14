f753ff65114ca1e0dd87c7295463dfc9fe79ad28
valsorym <valsorym.e@gmail.com>
2022-09-14 11:58:42 +0300

    Add base of the HTML-project

---
+++ 000-basic-structure/static/js/scripts.js
@@ -0,0 +1,7 @@
+// SCRIPTS
+// Write your JS code here.
+
+
+// Just test that the script is connected correctly.
+// TODO: Remove this line from production.
+console.log('The script works fine...');
